6e16eb9a20512a252e7c80c57b0d426acae690f6
dgrachov <dgrachov@icloud.com>
2026-04-20 17:44:04 +0200

    feat(parser): add ast and let statements

---
+++ monkey/parser/parser.go
@@ -0,0 +1,92 @@
+package parser
+
+import (
+	"github.com/dgrachov/monkey/monkey/ast"
+	"github.com/dgrachov/monkey/monkey/lexer"
+	"github.com/dgrachov/monkey/monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+
+	//program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		statement := p.parseStatement()
+
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.Let:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.Identifier) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.Assign) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+
+	for !p.curTokenIs(token.Semi) {
+		p.nextToken()
+	}
+
+	return statement
+}
+
+func (p *Parser) expectPeek(tokenType token.TokenType) bool {
+	if p.peekTokenIs(tokenType) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) curTokenIs(tokenType token.TokenType) bool {
+	return p.curToken.Type == tokenType
+}
+
+func (p *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
+}
---
+++ monkey/parser/parser_test.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dgrachov/monkey/monkey/ast"
+	"github.com/dgrachov/monkey/monkey/lexer"
+)
+
+func TestLetStatement(t *testing.T) {
+	input := `let x = 5;
+let y = 10;
+let foobar = 676767;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	statementCount := len(program.Statements)
+	if statementCount != 3 {
+		t.Fatalf("expected program.Statements to contain 3 statements. got=%d", statementCount)
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("tests[%d]", i), func(t *testing.T) {
+			testLetStatement(t, program.Statements[i], tt.expectedIdentifier)
+		})
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("expected s.TokenLiteral to be 'let'. got=%q", s.TokenLiteral())
+		return
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("expected *ast.LetStatement. got=%T", s)
+		return
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("expected s.Name.Value to be %s. got=%s", name, letStmt.Name.Value)
+		return
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("expected s.Name to be %s. got=%s", name, letStmt.Name)
+		return
+	}
+}
