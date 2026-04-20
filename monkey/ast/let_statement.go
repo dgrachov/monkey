4a9f0a6c0b7f321de23bf3e9c89b5be11a608673
dgrachov <dgrachov@icloud.com>
2026-04-20 18:25:16 +0200

    feat(ast): add `String` function to all nodes

--- monkey/ast/let_statement.go
+++ monkey/ast/let_statement.go
@@ -1,6 +1,10 @@
 package ast
 
-import "github.com/dgrachov/monkey/monkey/token"
+import (
+	"bytes"
+
+	"github.com/dgrachov/monkey/monkey/token"
+)
 
 type LetStatement struct {
 	Token token.Token
@@ -12,4 +16,21 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(ls.Name.String())
+
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
